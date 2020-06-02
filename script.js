91b6ada6a40ee23c9aa5be116a3f5fb703308017
Ahmed <aalkaysi@gmail.com>
2020-06-01 20:22:01 -0400

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const grid = document.getElementById('grid')
 const block = document.createElement('div')
 
+//add class to div
+block.className= 'gridbox'
 
-grid.appendChild(block)
+//create variable div tags
+function createDivs(){
+    let divs = []
+    
+    for (i=0; i<5; i++){
+        divs[i] = "blocks"
+    }
+    return divs
+}
 
+//for loop to create blocks
+for (i=0; i<1; i++){
+    let block = document.createElement('div')
+    block.className= 'gridbox'
+    grid.appendChild(block)
+}
+
+
+
+
+console.log(divs)
